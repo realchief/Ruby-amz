40d099f2b56bafecd8b456c3c7d2dd87e5a22758
mksm <mksm@iddqd.su>
2015-09-12 04:25:04 -0300

    Moving code into classes/organizing stuff

---
+++ lib/amz_bestsellers_bot/parser.rb
@@ -0,0 +1,10 @@
+module AMZBestSellersBot
+  class Parser
+
+    def self.parse_asins(html)
+      regex = /<li id="result_[0-9]{1,}" data-asin="([A-Z0-9]{10})"/
+      html.scan(regex).flatten
+    end
+
+  end
+end
--- lib/amz_bestsellers_bot/worker.rb
+++ lib/amz_bestsellers_bot/worker.rb
@@ -2,8 +2,8 @@ require 'sidekiq'
 require 'yaml'
 require 'logger'
 require 'active_record'
-require 'pry'
 require_relative 'probe'
+require_relative 'parser'
 
 LOG = Logger.new(STDOUT)
 LOG.datetime_format = "%Y-%m-%d %H:%M:%S"
@@ -16,34 +16,60 @@ ActiveRecord::Base.establish_connection(:development)
 
 require_relative 'best_seller'
 
-class Worker
+module AMZBestSellersBot
+  class Worker
 
-  include Sidekiq::Worker
+    include Sidekiq::Worker
 
-  def initialize
-    @probe = Probe.new
-  end
-
-  def perform(category, max_results)
-    page = 0
-    collected_results = []
-
-    while collected_results.size < max_results.to_i do
-      res = @probe.query(category, page)
-      LOG.info "Fetched page #{page}"
-
-      captcha_page = !!(res.body =~ /Type the characters you see in this image/i)
-      LOG.info "Fetched captcha page: #{captcha_page}"
-
-      results = res.body.scan(/<li id="result_[0-9]{1,}" data-asin="([A-Z0-9]{10})"/).flatten
-      LOG.info "Scraped #{results.size} products"
-
-      collected_results = (collected_results | results)
-      LOG.info "Collected results: #{collected_results.size}"
+    def initialize
+      @probe = Probe.new
     end
 
-    collected_results.each_with_index do |asin, pos|
-      BestSeller.create(asin: asin, position: pos + 1, category_id: category)
+    # Apparel & Accessories   (1036592)    Not found
+    # Arts, Crafts & Sewing   (2617941011) Done
+    # Automotive              (15684181)   Done
+    # Baby                    (165796011)  Done
+    # Beauty                  (3760911)    Done
+    # Camera & Photo          (502394)     Done
+    # Car Toys                (10963061)   Only 650 items
+    # Health & Personal Care  (3760901)    Done
+    # Home & Kitchen          (1055398)    Done
+    # Industrial & Scientific (16310091)   Done
+    # Jewelry                 (3367581)    Not found
+    # Kitchen & Dining        (284507)     Done
+    # Musical Instruments     (11091801)   Done
+    # Office Products         (1064954)    Done
+    # Pet Supplies            (2619533011) Done
+    # Shoes                   (672123011)  Not found
+    # Specialty Stores        (-4505)      Not found
+    # Sports & Outdoors       (3375251)    Done
+    # Tools & Hardware        (228013)     Done
+    # Toys and Games          (165793011)  Done
+    #
+    def perform(category, max_results)
+      page = 0
+      collected_results = []
+
+      while collected_results.size < max_results.to_i do
+        res = @probe.query(category, page += 1)
+        LOG.info "Fetched page #{page}"
+
+        captcha_page = !!(res.body =~ /Type the characters you see in this image/i)
+        LOG.info "Fetched captcha page: #{captcha_page}"
+
+        results = Parser.parse_asins(res.body)
+        LOG.info "Scraped #{results.size} products"
+
+        collected_results = (collected_results | results)
+        LOG.info "Collected results: #{collected_results.size}"
+      end
+
+      collected_results.each_with_index do |asin, pos|
+        bs = BestSeller.find_or_initialize_by(asin: asin, category_id: category)
+        bs.position   = pos + 1
+        bs.updated_at = DateTime.now
+        bs.save
+      end
     end
   end
 end
