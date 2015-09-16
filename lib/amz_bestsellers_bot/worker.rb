2153c1c73d40284a1b2fdb77ac6e94fb8bf9a551
mksm <mksm@iddqd.su>
2015-09-16 02:39:08 -0300

    Worker: prevent AR warnings and removing comment block

--- lib/amz_bestsellers_bot/worker.rb
+++ lib/amz_bestsellers_bot/worker.rb
@@ -11,6 +11,7 @@ LOG.datetime_format = "%Y-%m-%d %H:%M:%S"
 cfg_dir = File.expand_path('../../../config', __FILE__)
 db_cfg  = YAML.load(File.read(File.join(cfg_dir, 'database.yml')))
 
+ActiveRecord::Base.raise_in_transactional_callbacks = true
 ActiveRecord::Base.configurations = db_cfg
 ActiveRecord::Base.establish_connection(:development)
 
@@ -25,27 +26,6 @@ module AMZBestSellersBot
       @probe = Probe.new
     end
 
-    # Apparel & Accessories   (1036592)    Not found
-    # Arts, Crafts & Sewing   (2617941011) Done
-    # Automotive              (15684181)   Done
-    # Baby                    (165796011)  Done
-    # Beauty                  (3760911)    Done
-    # Camera & Photo          (502394)     Done
-    # Car Toys                (10963061)   Only 650 items
-    # Health & Personal Care  (3760901)    Done
-    # Home & Kitchen          (1055398)    Done
-    # Industrial & Scientific (16310091)   Done
-    # Jewelry                 (3367581)    Not found
-    # Kitchen & Dining        (284507)     Done
-    # Musical Instruments     (11091801)   Done
-    # Office Products         (1064954)    Done
-    # Pet Supplies            (2619533011) Done
-    # Shoes                   (672123011)  Not found
-    # Specialty Stores        (-4505)      Not found
-    # Sports & Outdoors       (3375251)    Done
-    # Tools & Hardware        (228013)     Done
-    # Toys and Games          (165793011)  Done
-    #
     def perform(category, max_results)
       page = 0
       collected_results = []
