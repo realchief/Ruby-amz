1cc7df7b135476c911fad736719524d24d00b555
mksm <mksm@iddqd.su>
2016-02-11 13:06:44 -0200

    Updated schema; renamed updated_at->created_at

--- db/migrate/001_create_best_sellers.rb
+++ db/migrate/001_create_best_sellers.rb
@@ -5,7 +5,7 @@ class CreateBestSellers < ActiveRecord::Migration
       t.column   :asin,        'CHAR(10)'
       t.integer  :category_id, limit: 8
       t.integer  :position,    limit: 2
-      t.datetime :updated_at,  null: false
+      t.datetime :created_at,  null: false
     end
   end
 end
